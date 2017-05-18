aba8c465a5ee95079f5f7e288c050f4b82077e3a
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-05-18 15:11:55 +0200

    feat(ktree.*): first try to build tree from sphere

--- src/ktree.c
+++ src/ktree.c
@@ -3,23 +3,61 @@
 p_tree new_tree () {
 	p_tree tree ;
 	
-	if ((tree = malloc (sizeof(p_tree))) == NULL) {
+	if ((tree = calloc (1, sizeof(t_tree))) == NULL) {
 		fprintf (stderr, "Not enough memory to create a tree (file:ktree.c)\n") ;
+    exit_game () ;
+		exit(0) ;
+	}
+	if ((tree->sub_tree = calloc (K, sizeof(t_tree))) == NULL) {
+		fprintf (stderr, "Not enough memory to create sub_tree field (file:ktree.c)\n") ;
+    exit_game () ;
 		exit(0) ;
 	}
 		
 	return tree ;
 }
-/*
-p_tree init_tree () {
 
 
-}*/
 
 void free_tree (p_tree tree) {
 
-	if (tree != NULL)
-		free_tree (*tree->sub_tree) ;
-	free (tree) ;
+	if (tree != NULL) {
+		free_tree (tree->sub_tree) ;
+    free (tree) ;
+  }
 }
+
+/* Balls */
+
+  switch (i) {
+    case :
+
+p_tree ball_to_tree_bis (int radius, int depth, p_tree tree, s_position_3D center, s_position_3D vertex1, s_position_3D vertex2) {
+  fprintf(stderr, "Depth = %d/%d\n", depth, MAX_DEPTH) ;
+  if (depth >= MAX_DEPTH)
+    return tree;
+  
+    
+  for (int i=0 ; i<K ; i++) {
+    if(is_out_of_the_ball (radius, depth, center, vertex2))
+      tree->sub_tree[i] = EMPTY ;
+    else if (is_in_the_ball (diameter, center, depth, accuracy))
+      tree->sub_tree[i] = FULL ;
+    else
+      tree->sub_tree[i] = ball_to_tree_bis (radius, ++depth, tree->sub_tree[i], center, vertex1, vertex2) ;
+  }
+  return tree;
+}
+
+
+p_tree ball_to_tree (int radius, t_position_3D center) {
+  p_tree tree ;
+  tree = new_tree () ;
+  
+  return ball_to_tree_bis (radius, 1, tree, center, center) ;
+}
+
+
+
+
 
--- src/ktree.h
+++ src/ktree.h
@@ -7,6 +7,14 @@
 #define K 8
 #define MAX_DEPTH 8
 
+#define LTF 0
+#define LTR 1
+#define LBF 2
+#define LBR 3
+#define RTF 4
+#define RTR 5
+#define RBF 6
+#define RBR 7
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,10 +23,12 @@ typedef struct s_tree t_tree ;
 typedef t_tree* p_tree ;
 
 struct s_tree {
-	p_tree *sub_tree ;
+        p_tree *sub_tree ;
 } ;
 
 p_tree new_tree () ;
-
+void free_tree (p_tree tree) ;
+p_tree ball_to_tree_bis (int diameter, int depth, p_tree tree, s_position_3D center) ;
+p_tree ball_to_tree (int diameter, s_position_3D center) ;
 
 #endif
