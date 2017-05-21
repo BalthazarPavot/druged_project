4f6b1dcbd8bcb7d3d4efe9ac58023756d3bb00c5
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-05-21 10:48:15 +0200

    creation of the k-tree algorithm for the spheres.

--- src/ktree.c
+++ src/ktree.c
@@ -10,16 +10,78 @@ p_tree new_tree () {
 		
 	return tree ;
 }
-/*
-p_tree init_tree () {
-
-
-}*/
 
 void free_tree (p_tree tree) {
 
-	if (tree != NULL)
-		free_tree (*tree->sub_tree) ;
-	free (tree) ;
+	if (tree != NULL) {
+    for (int i=0 ; i < K ; i++)
+      free_tree (tree->sub_tree[i]) ;
+    free (tree) ;
+  }
 }
 
+float distancef_3D (float x1, float y1, float z1,
+                    float x2, float y2, float z2 ) {
+  return sqrt (pow (x2 - x1, 2) + pow (y2 - y1, 2) + pow (z2 - z1, 2)) ;
+}
+
+void static _add_tree_to_object ( p_object_3D object,
+                                  char      (*status)(p_object_3D, float, float, float),
+                                  float       cube_x,
+                                  float       cube_y,
+                                  float       cube_z,
+                                  float       size,
+                                  char        depth) {
+  
+}
+
+
+char cube_status (p_object_3D object,
+                  char      (*status)(p_object_3D, float, float, float),
+                  float       cube_x,
+                  float       cube_y,
+                  float       cube_z,
+                  float       size) {
+  int x, y, z, sum ;
+
+  sum = 0 ;
+  for (x=0 ; x <= size ; x+=size) {
+    for (y=0 ; y <= size ; y+=size) {
+      for (z=0 ; z <= size ; z+=size) {
+        if (status (cube_x + x, cube_y + y, cube_z + z,
+            sphere->position.x, sphere->position.y,
+            sphere->position.z))
+          sum+=1 ;
+      }
+    }
+  }
+  
+  if (sum == 0)
+    return OUT ;
+  else if (sum == 8)
+    return INTO ;
+  return OVERLAP ;
+}
+
+char cube_status_sphere ( p_object_3D sphere,
+                          float       cube_x,
+                          float       cube_y,
+                          float       cube_z) {
+  return distancef_3D (
+    cube_x + x, cube_y + y, cube_z + z,
+    sphere->position.x, sphere->position.y,
+    sphere->position.z) < sphere->dimensions.radius ;
+}
+
+void add_tree_to_sphere (p_object_3D sphere) {
+
+  _add_tree_to_object (
+    sphere,
+    cube_status_sphere,
+    sphere->position.x - sphere->dimensions.radius, 
+    sphere->position.y - sphere->dimensions.radius, 
+    sphere->position.z - sphere->dimensions.radius, 
+    sphere->dimensions.radius * 2
+  ) ;
+
+}
--- src/ktree.h
+++ src/ktree.h
@@ -6,6 +6,10 @@
 
 #define K 8
 
+#define INTO 1
+#define OUT  2
+#define OVERLAP 3
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,10 +18,11 @@ typedef struct s_tree t_tree ;
 typedef t_tree* p_tree ;
 
 struct s_tree {
-	p_tree *sub_tree ;
+	p_tree sub_tree[K] ;
 } ;
 
 p_tree new_tree () ;
+void free_tree (p_tree) ;
 
 
 #endif
